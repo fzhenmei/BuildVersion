83a606c491de1867416d3956bee703e30c944c21
chris.zen <fzhenmei@gmail.com>
2011-07-19 21:17:44 +0800

    add demo projects

---
+++ CommonAssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("BulidVersion")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("cb5d9d04-06df-460f-9a3c-507df7ff6798")]
+
+[assembly: AssemblyVersion("2.3.4.5")]
+[assembly: AssemblyFileVersion("2.3.4.5")]
